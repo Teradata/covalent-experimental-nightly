764674968ef7b9874bfe102f9ec8a173adc176b4
Stoovm <stevenov7@users.noreply.github.com>
2018-06-27 20:48:36 +0000

    47b0b2f chore(): modify npm and nightly publish scripts for experimental package

---
+++ bundles/covalent-experimental.umd.js
@@ -0,0 +1,46 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('@angular/common')) :
+	typeof define === 'function' && define.amd ? define('@covalent/experimental', ['exports', '@angular/core', '@angular/common'], factory) :
+	(factory((global.covalent = global.covalent || {}, global.covalent.experimental = {}),global.ng.core,global.ng.common));
+}(this, (function (exports,core,common) { 'use strict';
+
+var TdRenameMeComponent = /** @class */ (function () {
+    function TdRenameMeComponent() {
+    }
+    return TdRenameMeComponent;
+}());
+TdRenameMeComponent.decorators = [
+    { type: core.Component, args: [{
+                selector: 'td-renameMe',
+                styles: [""],
+                template: "<a href=\"https://github.com/Teradata/covalent\"> Test Link </a>\n",
+            },] },
+];
+TdRenameMeComponent.ctorParameters = function () { return []; };
+var CovalentRenameMeModule = /** @class */ (function () {
+    function CovalentRenameMeModule() {
+    }
+    return CovalentRenameMeModule;
+}());
+CovalentRenameMeModule.decorators = [
+    { type: core.NgModule, args: [{
+                imports: [
+                    common.CommonModule,
+                ],
+                declarations: [
+                    TdRenameMeComponent,
+                ],
+                exports: [
+                    TdRenameMeComponent,
+                ],
+            },] },
+];
+CovalentRenameMeModule.ctorParameters = function () { return []; };
+
+exports.CovalentRenameMeModule = CovalentRenameMeModule;
+exports.TdRenameMeComponent = TdRenameMeComponent;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=covalent-experimental.umd.js.map
---
+++ esm5/covalent-experimental-template-rename-me-experiment-module.js
@@ -0,0 +1,38 @@
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+var TdRenameMeComponent = /** @class */ (function () {
+    function TdRenameMeComponent() {
+    }
+    return TdRenameMeComponent;
+}());
+TdRenameMeComponent.decorators = [
+    { type: Component, args: [{
+                selector: 'td-renameMe',
+                styles: [""],
+                template: "<a href=\"https://github.com/Teradata/covalent\"> Test Link </a>\n",
+            },] },
+];
+TdRenameMeComponent.ctorParameters = function () { return []; };
+var CovalentRenameMeModule = /** @class */ (function () {
+    function CovalentRenameMeModule() {
+    }
+    return CovalentRenameMeModule;
+}());
+CovalentRenameMeModule.decorators = [
+    { type: NgModule, args: [{
+                imports: [
+                    CommonModule,
+                ],
+                declarations: [
+                    TdRenameMeComponent,
+                ],
+                exports: [
+                    TdRenameMeComponent,
+                ],
+            },] },
+];
+CovalentRenameMeModule.ctorParameters = function () { return []; };
+
+export { CovalentRenameMeModule, TdRenameMeComponent };
+//# sourceMappingURL=covalent-experimental-template-rename-me-experiment-module.js.map
