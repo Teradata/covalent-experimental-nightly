72c4191a160f7cb8057112168b849074fd88cfbb
Christian Memije <christianmemije@users.noreply.github.com>
2019-10-03 22:53:10 +0000

    1aa7acb feat(): promote markdown-navigator aka help package (#1485)

--- esm2015/help/covalent-experimental-help.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/**
- * Generated bundle index. Do not edit.
- */
-export { CovalentHelpModule, HelpComponent, HelpWindowComponent, DraggableHelpWindowDialogService, DEFAULT_HELP_COMP_LABELS, DEFAULT_HELP_WINDOW_COMP_LABELS } from './index';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY292YWxlbnQtZXhwZXJpbWVudGFsLWhlbHAuanMiLCJzb3VyY2VSb290Ijoibmc6Ly9AY292YWxlbnQvZXhwZXJpbWVudGFsL2hlbHAvIiwic291cmNlcyI6WyJjb3ZhbGVudC1leHBlcmltZW50YWwtaGVscC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEsb0tBQWMsU0FBUyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBHZW5lcmF0ZWQgYnVuZGxlIGluZGV4LiBEbyBub3QgZWRpdC5cbiAqL1xuXG5leHBvcnQgKiBmcm9tICcuL2luZGV4JztcbiJdfQ==
