7a0d911fba9cddaaaeffcf91f6aa0a74ce31eb31
Christian Memije <7193975+christianmemije@users.noreply.github.com>
2020-04-27 18:17:15 +0000

    febf979 fix(coding-standards): fix install (#1742)

--- bundles/covalent-experimental-loading-mask.umd.js
+++ bundles/covalent-experimental-loading-mask.umd.js
@@ -46,7 +46,7 @@
             { type: core.Component, args: [{
                         selector: 'td-loading-mask',
                         template: "<span #animSpan></span>\n",
-                        styles: [".td-loading-mask-animated:host{-webkit-transform:translateZ(0);transform:translateZ(0)}:host{display:-webkit-inline-box;display:-ms-inline-flexbox;display:inline-flex;background-color:rgba(0,0,0,.051);position:relative;overflow:hidden;height:16px;width:90px}.td-loading-mask-animated:host span{height:100%;width:130%;background:-webkit-gradient(linear,left top,right top,from(rgba(250,250,250,.098)),color-stop(45%,rgba(0,0,0,.057)));background:linear-gradient(to right,rgba(250,250,250,.098),rgba(0,0,0,.057) 45%);display:block;position:absolute;left:-130%;-webkit-animation:1.9s linear infinite loading;animation:1.9s linear infinite loading}@-webkit-keyframes loading{0%{left:-130%}100%,35%{left:130%}}@keyframes loading{0%{left:-130%}100%,35%{left:130%}}"]
+                        styles: [".td-loading-mask-animated:host{transform:translateZ(0)}:host{display:-ms-inline-flexbox;display:inline-flex;background-color:rgba(0,0,0,.051);position:relative;overflow:hidden;height:16px;width:90px}.td-loading-mask-animated:host span{height:100%;width:130%;background:linear-gradient(to right,rgba(250,250,250,.098),rgba(0,0,0,.057) 45%);display:block;position:absolute;left:-130%;-webkit-animation:1.9s linear infinite loading;animation:1.9s linear infinite loading}@-webkit-keyframes loading{0%{left:-130%}100%,35%{left:130%}}@keyframes loading{0%{left:-130%}100%,35%{left:130%}}"]
                     }] }
         ];
         TdLoadingMaskComponent.propDecorators = {
